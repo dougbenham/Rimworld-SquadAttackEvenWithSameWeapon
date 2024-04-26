1fe58ac897bc98e7a530b91e780b28d9ca4f55f7
Doug Benham <douglasabenham@gmail.com>
2024-04-26 12:23:34 -0700

    Add project files.

---
+++ Main.cs
@@ -0,0 +1,27 @@
+﻿using HarmonyLib;
+using Verse;
+using RimWorld;
+
+namespace SquadAttackEvenWithSameWeapon
+{
+    [StaticConstructorOnStartup]
+    public static class Main
+    {
+        static Main()
+        {
+            Harmony instance = new Harmony("doug.squadattackevenwithsameweapon");
+            instance.PatchAll();
+        }
+    }
+
+    [HarmonyPatch(typeof(PawnAttackGizmoUtility), "AtLeastTwoSelectedPlayerPawnsHaveDifferentWeapons")]
+    public static class PawnAttackGizmoUtility_AtLeastTwoSelectedPlayerPawnsHaveDifferentWeapons_Patch
+    {
+	    [HarmonyPrefix]
+	    public static bool Prefix(ref bool __result)
+	    {
+		    __result = Find.Selector.NumSelected >= 2;
+		    return false;
+	    }
+    }
+}
